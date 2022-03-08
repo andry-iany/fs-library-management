e370a2fc9cd9e25c4ae1c8a9ade900d861b5a6c7
andry <andrynirotsy@gmail.com>
2022-03-08 17:47:42 +0300

    Allow only one CIN per account

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,13 @@
-const { formatResponse } = require("../utils");
+const { formatResponse, ErrorResponse } = require("../utils");
 const User = require("../models/User");
 
 const formatResponseSuccess = formatResponse.formatResponseSuccess;
 
 async function register(req, res, next) {
+	const userExist = await User.findOne({ CIN: req.validBody.CIN });
+	if (userExist)
+		return next(new ErrorResponse("L'utilisateur est dejà membre.", 400));
+
 	const user = new User({
 		...req.validBody,
 		dateDeNaissance: new Date(req.validBody.dateDeNaissance),
