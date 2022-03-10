a893d1dcd9c0b4e06ca971aed9bb5072a7f3feff
andry <andrynirotsy@gmail.com>
2022-03-10 17:44:46 +0300

    Client: refactor FormAddUser for the sake of Open/Closed

--- client/src/components/Main/Form/FormAddUser.js
+++ client/src/components/Main/Form/FormAddUser.js
@@ -2,76 +2,113 @@ import { SectionTitle } from "../../shared";
 import { Button, Form } from "react-bootstrap";
 import Alert from "../Alert";
 import { useState } from "react";
+import { usePost } from "../../../hooks/HTTPHooks";
 
 export default function FormAddUser() {
-	const [showAlert, setShowAlert] = useState(true);
+	const [showAlert, setShowAlert] = useState(false);
+	const { makeRequest, data, isPending, error } = usePost();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const formData = getFormData(e.target);
+		await makeRequest("/users/register", formData);
+		setShowAlert(() => true);
+	};
 
 	return (
 		<Form onSubmit={handleSubmit}>
 			<SectionTitle text="Ajouter un membre" />
 
 			<Alert
-				variant="danger"
-				text="this is the content"
+				variant={data ? "success" : "danger"}
+				text={data ? "Membre créer avec succès." : error}
 				show={showAlert}
 				closeAlert={() => setShowAlert(false)}
 			/>
 
-			<Form.Group controlId="nom" className="mb-3">
-				<Form.Label>Nom du nouveau membre:</Form.Label>
-				<Form.Control
-					required
-					name="nom"
-					placeholder="Veuillez saisir le nom"
-				/>
-			</Form.Group>
-
-			<Form.Group controlId="naissance" className="mb-3">
-				<Form.Label>Date de naissance:</Form.Label>
-				<Form.Control type="date" required name="naissance" />
-			</Form.Group>
-
-			<Form.Group controlId="CIN" className="mb-3">
-				<Form.Label>Carte d'identité nationale du membre ou tuteur:</Form.Label>
-				<Form.Control
-					required
-					name="CIN"
-					placeholder="Veuillez saisir la CIN"
-				/>
-			</Form.Group>
-
-			<Form.Group className="mb-3">
-				<Form.Label>Adresse:</Form.Label>
-				<Form.Control
-					className="mb-2"
-					required
-					name="logement"
-					placeholder="Logement (eg: IAD 90A)"
-				/>
-				<Form.Control
-					className="mb-2"
-					required
-					name="commune"
-					placeholder="Commune"
-				/>
-				<Form.Control
-					className="mb-2"
-					required
-					name="ville"
-					placeholder="Ville"
-				/>
-			</Form.Group>
+			{getFormGroups()}
 
 			<div className="mt-4 d-flex justify-content-center">
-				<Button type="submit" className="btn-primary-cust">
-					Soumettre
+				<Button type="submit" className="btn-primary-cust" disabled={isPending}>
+					{isPending ? "Patientez..." : "Soumettre"}
 				</Button>
 			</div>
 		</Form>
 	);
 }
 
-function handleSubmit(e) {
-	e.preventDefault();
-	alert("we are adding new user.");
+function getFormData(form) {
+	return {
+		nom: form.nom.value,
+		dateDeNaissance: form.naissance.value,
+		CIN: form.CIN.value,
+		adresse: {
+			logement: form.logement.value,
+			commune: form.commune.value,
+			ville: form.ville.value,
+		},
+	};
+}
+
+function getFormGroups() {
+	const groups = [
+		{
+			label: "Nom du nouveau membre:",
+			formControls: [
+				{
+					name: "nom",
+					placeholder: "Veuillez saisir le nom",
+				},
+			],
+		},
+		{
+			label: "Date de naissance:",
+			formControls: [
+				{
+					name: "naissance",
+					placeholder: "Date de naissance",
+					type: "date",
+				},
+			],
+		},
+		{
+			label: "Carte d'identité du membre ou celui du tuteur:",
+			formControls: [
+				{
+					name: "CIN",
+					placeholder: "Veuillez saisir la CIN",
+				},
+			],
+		},
+		{
+			label: "Adresse:",
+			formControls: [
+				{ name: "logement", placeholder: "Logement (eg: IAD 90A)" },
+				{ name: "commune", placeholder: "Commune" },
+				{ name: "ville", placeholder: "Ville" },
+			],
+		},
+	];
+
+	return groups.map((group) => {
+		return (
+			<Form.Group className="mb-3" key={group.label}>
+				<Form.Label>{group.label}</Form.Label>
+				{group.formControls.map((fc) => getFormControl(fc))}
+			</Form.Group>
+		);
+	});
+}
+
+function getFormControl({ name, placeholder, type }) {
+	return (
+		<Form.Control
+			className="mb-2"
+			required
+			name={name}
+			placeholder={placeholder}
+			key={name}
+			type={type || "text"}
+		/>
+	);
 }
