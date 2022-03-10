2d86c6c0b6d1ea1a67e99272da8be05d4a372cde
andry <andrynirotsy@gmail.com>
2022-03-10 20:28:48 +0300

    Client: implement return

--- client/src/components/Main/Form/FormRentalReturn.js
+++ client/src/components/Main/Form/FormRentalReturn.js
@@ -1,14 +1,41 @@
 import FormRental from "./FormRental";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePost } from "../../../hooks/HTTPHooks";
 
 export default function FormRentalReturn() {
-	const [showAlert, setShowAlert] = useState(true);
+	const [showAlert, setShowAlert] = useState(false);
+	const { makeRequest, error, data, isPending } = usePost();
+	const [alertTimeout, setAlertTimeout] = useState(null);
+
+	useEffect(() => {
+		if (data) clearForm();
+	}, [data]);
+
+	useEffect(() => {
+		return () => clearTimeout(alertTimeout);
+	});
+
+	const hideAlertAfterTimeout = () => {
+		const timeout = setTimeout(() => {
+			setShowAlert(() => false);
+			setAlertTimeout(() => null);
+		}, 5000);
+		setAlertTimeout(timeout);
+	};
 
 	const alertProps = {
-		text: "Une erreur ou peut etre pas.",
-		variant: "danger",
 		show: showAlert,
 		closeAlert: () => setShowAlert(false),
+		variant: data ? "success" : "danger",
+		text: data ? "Opération réussie." : error,
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const formData = getFormData(e.target);
+		await makeRequest("/rental/return", formData);
+		setShowAlert(() => true);
+		hideAlertAfterTimeout();
 	};
 
 	return (
@@ -16,11 +43,26 @@ export default function FormRentalReturn() {
 			title="Retourner un livre"
 			handleSubmit={handleSubmit}
 			alertProps={alertProps}
+			isPending={isPending}
 		/>
 	);
 }
 
-function handleSubmit(e) {
-	e.preventDefault();
-	alert("renting something");
+function clearForm() {
+	const form = document.querySelector("form");
+	form.userId.value = "";
+	Array.from(form.querySelectorAll(".ISBNInput input")).forEach(
+		(input) => (input.value = "")
+	);
+}
+
+function getFormData(form) {
+	const ISBNs = Array.from(form.querySelectorAll(".ISBNInput input"))
+		.map((input) => input.value)
+		.filter((value) => value.trim() !== "");
+
+	return {
+		userId: form.userId.value,
+		ISBN: ISBNs,
+	};
 }
