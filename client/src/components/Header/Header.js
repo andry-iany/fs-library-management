5c906c53d6e0f70e8e6fa47fbedefb6d9ac402b2
andry <andrynirotsy@gmail.com>
2022-03-11 13:11:23 +0300

    Client: add nav dropdown

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,22 @@
 import ShadowedBox from "../shared/ShadowedBox";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function Header() {
 	return (
-		<ShadowedBox className="h1 mb-5">
-			<span>Biblio</span>
-			<span className="text-primary-cust">Tech</span>
+		<ShadowedBox className="mb-5">
+			<Navbar className="d-flex justify-content-between align-items-end  p-0">
+				<Navbar.Brand>
+					<span className="h1 text-secondary">Biblio</span>
+					<span className="h1 text-primary-cust">Tech</span>
+				</Navbar.Brand>
+				<Nav className="text-primary-cust">
+					<NavDropdown title="Option" className="h5" align="end">
+						<NavDropdown.Item href="#action/3.1">
+							Se déconnecter
+						</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Navbar>
 		</ShadowedBox>
 	);
 }
