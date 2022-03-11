b2bc4b7ffe133fdd3130f056a2f4f6fa36dfd42c
andry <andrynirotsy@gmail.com>
2022-03-11 07:18:47 +0300

    Client: refactor HTTPHooks
    create generic hook to make HTTPRequest and change useGet and usePost to use this hook

--- client/src/hooks/HTTPHooks.js
+++ client/src/hooks/HTTPHooks.js
@@ -4,36 +4,24 @@ import { useState } from "react";
 const apiRoot = "http://localhost:8080";
 
 function usePost() {
-	const [data, setData] = useState(null);
-	const [error, setError] = useState(null);
-	const [isPending, setIsPending] = useState(false);
-
-	const makeRequest = async (path, body) => {
-		setIsPending(() => true);
-		try {
-			const result = await axios.post(apiRoot + path, body);
-			setIsPending(() => false);
-			setError(() => null);
-			setData(() => result);
-		} catch (err) {
-			setIsPending(() => false);
-			setData(() => null);
-			setError(() => err?.response?.data?.error || "Une erreur est survenue.");
-		}
-	};
-
-	return { data, error, isPending, makeRequest };
+	const result = useMakeHttpRequest("post");
+	return result;
 }
 
 function useGet() {
+	const result = useMakeHttpRequest("get");
+	return result;
+}
+
+function useMakeHttpRequest(method = "get") {
 	const [data, setData] = useState(null);
 	const [error, setError] = useState(null);
 	const [isPending, setIsPending] = useState(false);
 
-	const makeRequest = async (path) => {
+	const makeRequest = async (path, ...rest) => {
 		setIsPending(() => true);
 		try {
-			const result = await axios.get(apiRoot + path);
+			const result = await axios[method](apiRoot + path, ...rest);
 			setIsPending(() => false);
 			setError(() => null);
 			setData(() => result);
