174a1a3f26b12c47c81dc1caa967e5c14a619259
andry <andrynirotsy@gmail.com>
2022-03-15 14:55:40 +0300

    Server: refactor auth controller
    - Remove unnecessary try/catch
    - Remove unneeded sendToken() function
    - Make direct use of formatResponse.forSuccess

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,29 +1,20 @@
 const { Admin } = require("../models");
 const { ErrorResponse, formatResponse } = require("../utils");
 
-const forSuccess = formatResponse.forSuccess;
-
 exports.loginController = async function (req, res, next) {
-	try {
-		const admin = await Admin.findOne({ email: req.validBody.email }).select(
-			"+password"
-		);
+	const admin = await Admin.findOne({ email: req.validBody.email }).select(
+		"+password"
+	);
+	if (!admin)
+		return next(new ErrorResponse("Email ou mot de passe incorrect.", 404));
 
-		if (!admin)
-			return next(new ErrorResponse("Email ou mot de passe incorrect.", 404));
+	const isMatch = await admin.matchPasswords(req.validBody.password);
+	if (!isMatch)
+		return next(new ErrorResponse("Email ou mot de passe incorrect.", 404));
 
-		const isMatch = await admin.matchPasswords(req.validBody.password);
-
-		if (!isMatch)
-			return next(new ErrorResponse("Email ou mot de passe incorrect.", 404));
-
-		return sendToken(admin, 200, res);
-	} catch (err) {
-		return next(err);
-	}
-};
-
-const sendToken = (admin, statusCode, res) => {
 	const token = admin.getSignedToken();
-	return res.status(statusCode).json(forSuccess({ token, role: admin.role }));
+
+	return res
+		.status(200)
+		.json(formatResponse.forSuccess({ token, role: admin.role }));
 };
